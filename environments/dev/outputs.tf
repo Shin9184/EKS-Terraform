e74f0e587253d6096c457eb66480f1b9237af6b8
Shin9184 <sds38839184@gmail.com>
2024-03-22 12:07:39 +0900

    Terraform 을 통한 EKS 생성 모듈

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,15 @@
+# output "vpc_id" {
+#   value = module.vpc.vpc_id
+# }
+
+# output "subnet_ids" {
+#   value = module.vpc.subnet_ids
+# }
+
+# output "eks_sg_id" {
+#   value = module.ec2.eks_sg_id
+# }
+
+# output "subnets_private_ids" {
+#   value = values(aws_subnet.subnets_private)[*].id
+# }
